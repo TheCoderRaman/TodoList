9908af8df83c59eccca5546f182c68623e2d61cd
Raman Verma <devramanverma@gmail.com>
2023-07-19 19:20:05 +0530

    Added CloseIcon.js

---
+++ src/Components/Icons/CloseIcon.js
@@ -0,0 +1,26 @@
+import * as React from "react";
+
+export const CloseIcon = ({
+  fill = "currentColor",
+  filled,
+  size,
+  height,
+  width,
+  label,
+  ...props
+}) => {
+  return (
+    <svg
+      width={size || width || 24}
+      height={size || height || 24}
+      viewBox="0 0 24 24"
+      fill={filled ? fill : "none"}
+      xmlns="http://www.w3.org/2000/svg"
+      {...props}
+    >
+      <path
+        d="M12 2a10 10 0 1010 10A10.016 10.016 0 0012 2zm3.36 12.3a.754.754 0 010 1.06.748.748 0 01-1.06 0l-2.3-2.3-2.3 2.3a.748.748 0 01-1.06 0 .754.754 0 010-1.06l2.3-2.3-2.3-2.3A.75.75 0 019.7 8.64l2.3 2.3 2.3-2.3a.75.75 0 011.06 1.06l-2.3 2.3z"
+      />
+    </svg>
+  );
+};
