43b1e9ae91de3e15c4fc6305e2700c6d01ee91e4
Raman Verma <devramanverma@gmail.com>
2023-07-19 18:26:40 +0530

    Added translations.js for defining all application locales

---
+++ src/Data/translations.js
@@ -0,0 +1,20 @@
+import { en } from './Locales/en';
+import { hi } from './Locales/hi';
+
+/**
+ * Application translation locales.
+ * 
+ * Here we define translations for our entrie app.
+ * 
+ * conventions:
+ * - @name Name of the file
+ * - @file Exact path of the file from the source root
+ */
+export default {
+  en: {
+    translation: en,
+  },
+  hi: {
+    translation: hi,
+  },
+};
