72081e04e4fd3c91c484c8de09ba06932c00c4a8
Raman Verma <devramanverma@gmail.com>
2023-07-19 18:27:52 +0530

    Added paginatorConfig.js for table pagination

---
+++ src/Data/Table/paginateConfig.js
@@ -0,0 +1,10 @@
+/**
+ * Table paginate config for pagination.
+ * 
+ * @var const paginateConfig
+ */
+export const paginateConfig = {
+    items: 0, pages: 0,
+    pageSize: 10, pageNumber: 1,
+    pageSizes: [10, 20, 30, 40, 50],
+};
